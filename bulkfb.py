dda5e3f8dd95e5b5dabcde37a3d45631eb41fe01
Manish Fale <manishfale33@gmail.com>
2023-09-16 00:27:08 +0530

    16/09/23

--- bulkfb.py
+++ bulkfb.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
 def send_facebook_messages():
     username = username_entry.get()
     password = password_entry.get()
     recipient = recipient_entry.get()
-    message_text = message_entry.get("1.0", tk.END) 
+    message_text = message_entry.get("1.0", tk.END)
 
     # Set up the Chrome WebDriver
     driver = webdriver.Chrome()
@@ -17,27 +20,32 @@ def send_facebook_messages():
         driver.get("https://www.facebook.com")
 
         # Find and interact with the email/phone and password fields
-        email_field = driver.find_element_by_id("email")
+        email_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "email")))
         email_field.send_keys(username)
 
-        password_field = driver.find_element_by_id("pass")
+        password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "pass")))
         password_field.send_keys(password)
 
-        login_button = driver.find_element_by_id("loginbutton")
+        login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "login")))
         login_button.click()
 
         # Wait for login to complete
-        driver.implicitly_wait(10)
+        WebDriverWait(driver, 20).until(EC.url_contains("https://www.facebook.com/"))
+
+        # Check if the login page reopens
+        if "login" in driver.current_url:
+            print("Login failed. Please check your credentials.")
+            return
 
         # Go to the recipient's profile (replace with the recipient's profile URL)
         driver.get(f"https://www.facebook.com/{recipient}")
 
-        # Click on the message button
-        message_button = driver.find_element_by_css_selector("a[data-testid='chat_sidebar']")
+        # Click on the message button (if it doesn't work, you may need to adjust this)
+        message_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[data-testid='chat_sidebar']")))
         message_button.click()
 
         # Enter the message and send
-        message_input = driver.find_element_by_css_selector("div[role='combobox']")
+        message_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[contenteditable='true']")))
         message_input.send_keys(message_text)
         message_input.send_keys(Keys.RETURN)
 
